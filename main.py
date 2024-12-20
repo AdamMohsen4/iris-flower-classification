2a5935e24e1d9426ba216f63fd8733d5928067b0
AdamMohsen4 <adam.amohsen4@gmail.com>
2024-12-21 00:15:54 +0100

    Load Iris dataset and display initial records

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt # data visualization
 import seaborn as sns # data visualization
+
+# Load the data
+columns = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width', 'Class_labels'] # Column names
+df = pd.read_csv('iris.data', names = columns) 
+df.head()
