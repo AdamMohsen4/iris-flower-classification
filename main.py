47b05f3562ef3378b5b259b2232ee06a56b6087a
AdamMohsen4 <adam.amohsen4@gmail.com>
2024-12-21 00:34:36 +0100

    Fix feature-target separation and add model training split for Iris dataset

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ X = data[:, 0:4] # Features are the first 4 columns (Sepal length, Sepal width, 
 y = data[:, 4] # Target is the last column (Class labels)
 
 # Calculate average of each features for all classes
-Y_Data = np.array([np.average(X[:, i][Y==j].astype('float32')) for i in range (X.shape[1])
- for j in (np.unique(Y))])
+Y_Data = np.array([np.average(X[:, i][y==j].astype('float32')) for i in range (X.shape[1])
+ for j in (np.unique(y))])
 Y_Data_reshaped = Y_Data.reshape(4, 3)
 Y_Data_reshaped = np.swapaxes(Y_Data_reshaped, 0, 1)
 X_axis = np.arange(len(columns)-1)
@@ -36,3 +36,8 @@ plt.xlabel("Features")
 plt.ylabel("Value in cm.")
 plt.legend(bbox_to_anchor=(1.3,1))
 plt.show()
+
+# Model training
+# Split the data to train and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
