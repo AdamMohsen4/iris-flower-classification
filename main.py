26b150509efc9412941c05200359c07cef4baec6
AdamMohsen4 <adam.amohsen4@gmail.com>
2024-12-21 00:31:36 +0100

    Enhance feature-target separation and add bar plot for average feature values across classes

--- main.py
+++ main.py
@@ -14,7 +14,25 @@ df.describe() # Summary statistics
 # Visualize the whole dataset
 sns.pairplot(df, hue = 'Class_labels')
 
-# Separate features and target
+# Separate features and target because we need to feed the features to the model to make predictions
 data = df.values # Convert the dataframe to a numpy array
 X = data[:, 0:4] # Features are the first 4 columns (Sepal length, Sepal width, Petal length, Petal width)
 y = data[:, 4] # Target is the last column (Class labels)
+
+# Calculate average of each features for all classes
+Y_Data = np.array([np.average(X[:, i][Y==j].astype('float32')) for i in range (X.shape[1])
+ for j in (np.unique(Y))])
+Y_Data_reshaped = Y_Data.reshape(4, 3)
+Y_Data_reshaped = np.swapaxes(Y_Data_reshaped, 0, 1)
+X_axis = np.arange(len(columns)-1)
+width = 0.25
+
+# Plot the average
+plt.bar(X_axis, Y_Data_reshaped[0], width, label = 'Setosa')
+plt.bar(X_axis+width, Y_Data_reshaped[1], width, label = 'Versicolour')
+plt.bar(X_axis+width*2, Y_Data_reshaped[2], width, label = 'Virginica')
+plt.xticks(X_axis, columns[:4])
+plt.xlabel("Features")
+plt.ylabel("Value in cm.")
+plt.legend(bbox_to_anchor=(1.3,1))
+plt.show()
